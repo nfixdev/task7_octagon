8898249c7932aaa30f834f068e23366f48ef7b64
nfixdev <150429550+nfixdev@users.noreply.github.com>
2023-12-12 20:33:13 +1200

    main

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bot = new telegrambot(token, {polling: true});
 bot.onText(/^\!qr/, function(msg) {
     console.log(msg);
     var userId = msg.from.id;
-    var data = msg.text.substring(3).trim();
+    var data = msg.text.substring(3);
     var imageqr = "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=" + data;
     bot.sendMessage(msg.chat.id, imageqr);
 });
@@ -15,7 +15,7 @@ bot.onText(/^\!qr/, function(msg) {
 bot.onText(/^\!webscr/, function(msg) {
     console.log(msg);
     var userId = msg.from.id;
-    var url = msg.text.substring(8).trim();
+    var url = msg.text.substring(8);
     var image = "https://api.screenshotone.com/take?access_key=UrU5vvFY6zCM6Q&url=" + url + "&viewport_width=1280&viewport_height=720&device_scale_factor=1";
     bot.sendMessage(msg.chat.id, image);
   });
